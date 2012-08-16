c1210a0980189ff437fc3211d983f893bd853922
Abraham Walters <quidmonkey@gmail.com>
2012-08-16 17:31:42 -0400

    Initial Commit.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ SimplePythonHTTPServerBoilerplate
 =================================
 
 A simple HTTP Server written in Python 3.2.3
+
+This server is designed to run locally on the port of your choice:
+```python
+python server.py 80
+```
+If no port is specified, the server will default to port 8080.
+
+If a specific .html file is requested, the server will serve it up; otherwise, the server will serve up a generic 'Hello World!' message. Additional GET requests on specific filetypes can be added like so:
+```python
+if self.path.endswith('.ext'):
+	#serve up GET request here
+```
+
+The server is also designed to take form & url-encoded POST requests. The resolve_post_form() and resolve_post_url() methods can be overloaded with the desired logic.
+
+Feel free to fork and improve as you see fit.
+
+Enjoy!
---
+++ server.py
@@ -0,0 +1,79 @@
+import http.server
+from os import curdir, sep
+import socketserver
+import sys
+
+DEFAULT_PORT = 8080
+
+class mHandler(http.server.BaseHTTPRequestHandler):
+
+	def do_HEAD(self):
+		self.write_html_header()
+		return
+
+	def do_GET(self):
+		try:
+			if self.path.endswith('.html'):
+				f = open(curdir + sep + self.path)
+				self.write_html_header()
+				#write to output stream from .html file
+				#encode string to utf-8 charset as Python 3.x IO expects bytes
+				self.wfile.write(f.read().encode('utf-8'))
+				f.close()
+				return
+			else:
+				self.write_html_header()
+				#write standard greeting to output stream
+				#encode string to utf-8 charset as Python 3.x IO expects bytes
+				self.wfile.write('Hello World!'.encode('utf-8'))
+				return
+		except IOError:
+			self.send_error(404, 'File Not Found: %s' % self.path)
+
+	# from http://stackoverflow.com/questions/4233218/python-basehttprequesthandler-post-variables
+	def do_POST(self):
+		ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+		#parse form
+		if ctype == 'multipart/form-data':
+			post_params = cgi.parse_multipart(self.rfile, pdict)
+			resolve_post_form(post_params)
+        #parse url
+		elif ctype == 'application/x-www-form-urlencoded':
+			length = int(self.headers.getheader('content-length'))
+			post_params = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+			resolve_post_url(post_params)
+		#nothing to parse
+		else:
+			postvars = {}
+		self.send_response(301)
+		return
+
+	def resolve_post_form(self, form_params):
+		#do fancy stuff here with form data
+		pass
+
+	def resolve_post_url(self, url_params):
+		#do fancy stuff here with url data
+		pass
+
+
+	def write_html_header(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/html')
+		self.end_headers()
+		return
+
+def httpd(args=sys.argv):
+	try:
+		#grab port from cmd line
+		port = DEFAULT_PORT if len(args) < 1 else int(args[1])
+		server = http.server.HTTPServer(('', port), mHandler)
+		print('Running HTTP Server on Port', port)
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print('Shutting Down Server...')
+		server.socket.close()
+
+#entry point
+if __name__ == '__main__':
+	httpd()
