5f4490afdc7317f78b733887282b090ca05e88de
quidmonkey <quidmonkey@gmail.com>
2012-08-23 16:42:54 -0300

    Update server.py

--- server.py
+++ server.py
@@ -59,7 +59,7 @@ class mHandler(http.server.BaseHTTPRequestHandler):
 		if ctype == 'multipart/form-data':
 			post_params = cgi.parse_multipart(self.rfile, pdict)
 			resolve_post_form(post_params)
-        #parse url
+		#parse url
 		elif ctype == 'application/x-www-form-urlencoded':
 			length = int(self.headers.getheader('content-length'))
 			post_params = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
